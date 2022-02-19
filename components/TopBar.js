a1ace31d105ef0ba56e2676e0aa081572605050b
Jack Skiles <jackskiles420@gmail.com>
2022-02-18 20:08:56 -0500

    finished top bar

--- components/TopBar.js
+++ components/TopBar.js
@@ -13,12 +13,27 @@ const TopBar = () => {
                         <p>JackSkiles71@gmail.com</p>
                     </div>
                     <div className={styles.contactdiv}>
-                        <img className={styles.topimg} src="Icon ionic-ios-mail.png" />
+                        <img className={styles.topimg} src="Icon awesome-phone-alt.png" />
                         <p>JackSkiles71@gmail.com</p>
                     </div>
                 </div>
+                <div className={styles.seperatordiv}></div>
                 <div className={styles.topbarright2}>
-
+                    <div className={styles.contactdiv}>
+                            <img className={styles.topimg2} src="Icon awesome-linkedin.png" />
+                    </div>
+                    <div className={styles.contactdiv}>
+                            <img className={styles.topimg2} src="Icon awesome-github.png" />
+                    </div>
+                    <div className={styles.contactdiv}>
+                            <img className={styles.topimg2} src="Icon ionic-md-paper.png" />
+                    </div>
+                    <div className={styles.contactdiv2}>
+                            <img className={styles.topimg2mobile} src="mailmobile.png" />
+                    </div>
+                    <div className={styles.contactdiv2}>
+                            <img className={styles.topimg2mobile} src="phonemobile.png" />
+                    </div>
                 </div>
             </div>
         </nav>
