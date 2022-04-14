6dc3ece5b5653ebfa7a19a3faad4c4e72201e535
Jack Skiles <jackskiles420@gmail.com>
2022-04-14 11:32:21 -0400

    a lot of updates

--- components/Hero.js
+++ components/Hero.js
@@ -3,14 +3,17 @@ import { useState } from 'react';
 
 export default function Hero() {
     const [portrait, setImg] = useState("Mask Group 4.png");
+    const [classN, setClass] = useState("portrait");
 
 
     const handleMouseEnter = () => {
         console.log("hello");
-        setImg("");
+        setImg("profilepic.png");
+        setClass("portrait2");
     }
     const handleMouseLeave = () => {
         setImg("Mask Group 4.png");
+        setClass("portrait");
     }
 
     return (
@@ -18,12 +21,23 @@ export default function Hero() {
             <div className='hero-Main'>
                 <div className='hero-inner' >
                     <div className='hero-left'>
-
+                        <div className='list-div1'>
+                            <img src="Rectangle 17.png" className="squareIcon"></img>
+                            <h2 className='list-header1'>SOFTWARE ENGINEER</h2>
+                        </div>
+                        <div className='list-div1'>
+                            <img src="Rectangle 17.png" className="squareIcon"></img>
+                            <h2 className='list-header2'>SOFTWARE ENGINEER</h2>
+                        </div>
+                        <div className='list-div1'>
+                            <img src="Rectangle 17.png" className="squareIcon"></img>
+                            <h2 className='list-header3'>SOFTWARE ENGINEER</h2>
+                        </div>
                     </div>
                     <div className='hero-right'>
                         <div className='hero-img-div' onMouseEnter={handleMouseEnter}
                                 onMouseLeave={handleMouseLeave} >
-                            <img src={portrait} className="portrait"  />
+                            <img src={portrait} className={classN}  />
                             {/* <button>Hello</button> */}
                         </div>
                     </div>
