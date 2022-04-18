ddc0e9561b5bb4f3ef73ba23a798060406b528f5
Jack Skiles <jackskiles420@gmail.com>
2022-04-18 16:26:36 -0400

    some updates to content

--- components/About.js
+++ components/About.js
@@ -4,11 +4,11 @@ import { useState, useEffect } from 'react';
 export default function About() {
   return (
     <div className="about-main">
-        <div className="about-column">
-            <div className="about-box">
-                
-            </div>
+      <div className="about-column">
+        <div className="about-box">
+          I'm a software engineer and designer from Chattanooga, TN. I have been working on websites, software and graphic design over the past 3 years. Since then I have learned many new technologies, languages and techniques to further the craft. I had a lot of fun making this site and the projects presented, so I hope you enjoy!
         </div>
+      </div>
     </div>
   )
 }
