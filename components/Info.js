62beff9be803bfe627ec5eacff306de4be18437e
Jack Skiles <jackskiles420@gmail.com>
2022-05-19 11:44:24 -0400

    css updates to devicons

--- components/Info.js
+++ components/Info.js
@@ -7,39 +7,39 @@ export default function Info() {
                 <div className="infoBox">
                     <div className="infoHeaderBox">
                         <div className='icon-div1-front'></div>
-                        <h3 className="projectHeader">Skills and Technologies</h3>
+                        <h3 className="infoHeader">Skills and Technologies</h3>
                     </div>
                     <div className="icon-container">
                         <div className="icon2">
-                            <i class="devicon-visualstudio-plain" id="skill"></i>
+                            <i class="devicon-visualstudio-plain" title="Visual Studio Code" id="skill"></i>
                             {/* <p id="iconText">Visual Studios</p> */}
                         </div>
                         <div className="icon2">
-                            <i className="devicon-postgresql-plain" id="skill"></i>
-                            {/* <p id="iconText">PostgreSQL</p> */}
-                        </div>
-                        <div className="icon2">
-                            <i class="devicon-sequelize-plain" id="skill"></i>
-                            {/* <p id="iconText">Sequelize</p> */}
-                        </div>
-                        <div className="icon2">
-                            <i class="devicon-nodejs-plain" id="skill"></i>
-                            {/* <p id="iconText">Node.js</p> */}
-                        </div>
-                        <div className="icon2">
-                            <i className="devicon-javascript-plain" id="skill"></i>
+                            <i className="devicon-javascript-plain" title="JavaScript" id="skill"></i>
                             {/* <p id="iconText">JavaScript</p> */}
                         </div>
                         <div className="icon2">
-                            <i className="devicon-html5-plain" id="skill"></i>
+                            <i className="devicon-postgresql-plain" title="PostgreSQL" id="skill"></i>
+                            {/* <p id="iconText">PostgreSQL</p> */}
+                        </div>
+                        <div className="icon2">
+                            <i class="devicon-sequelize-plain" title="Sequelize" id="skill"></i>
+                            {/* <p id="iconText">Sequelize</p> */}
+                        </div>
+                        <div className="icon2">
+                            <i class="devicon-nodejs-plain" title="Nodejs" id="skill"></i>
+                            {/* <p id="iconText">Node.js</p> */}
+                        </div>
+                        <div className="icon2">
+                            <i className="devicon-html5-plain" title="HTML5" id="skill"></i>
                             {/* <p id="iconText">HTML5</p> */}
                         </div>
                         <div class="icon2">
-                            <i class="devicon-git-plain" id="skill"></i>
+                            <i class="devicon-git-plain" title="Git" id="skill"></i>
                             {/* <p id="iconText">Git</p> */}
                         </div>
                         <div class="icon2">
-                            <i class="devicon-bootstrap-plain" id="skill"></i>
+                            <i class="devicon-bootstrap-plain" title="Bootstrap" id="skill"></i>
                             {/* <p id="iconText">Bootstrap</p> */}
                         </div>
                     </div>
