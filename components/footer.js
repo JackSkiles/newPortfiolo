288621206ce274aa5547644a8a4a4259b3450369
Jack Skiles <jackskiles420@gmail.com>
2022-05-23 12:10:39 -0400

    lots of css updates

--- components/footer.js
+++ components/footer.js
@@ -3,19 +3,26 @@ import React from 'react'
 export default function footer() {
   return (
     <div>
-        <div className="footerMain">
-            <div className="footerLeft">
-                <h2 className="footerHeader">
-                    Contact
-                </h2>
-                <a href="+14236195332" className="footerLink">(423) 619-5332</a>
-                <a href="jackskiles71@gmail.com" className="footerLink">jackskiles71@gmail.com</a>
-            </div>
-            <div className="footerCenter">
-            </div>
-            <div className="footerRight">
-            </div>
+      <div className="footerMain">
+        <div className="footerLeft">
+          <h2 className="footerHeader">
+            Contact
+          </h2>
+          <a href="+14236195332" className="footerLink">(423) 619-5332</a>
+          <a href="jackskiles71@gmail.com" className="footerLink">jackskiles71@gmail.com</a>
         </div>
+        <div className="footerCenter">
+
+        </div>
+        <div className="footerRight">
+          {/* <h2 className="footerHeader">
+            Contact
+          </h2> */}
+          {/* <a href="+14236195332" className="footerLink">(423) 619-5332</a>
+          <a href="jackskiles71@gmail.com" className="footerLink">jackskiles71@gmail.com</a> */}
+          <h4 className="designedBy">Created by <br /> Jack Skiles</h4>
+        </div>
+      </div>
     </div>
   )
 }
