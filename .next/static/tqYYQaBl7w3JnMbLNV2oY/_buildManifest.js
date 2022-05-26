ecace6f1b97591e4a20ddc1e3af60f7541d27122
Jack Skiles <jackskiles420@gmail.com>
2022-05-26 14:01:48 -0400

    updates

--- .next/static/tqYYQaBl7w3JnMbLNV2oY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ef46db3751d8e999.css","static/chunks/pages/index-5c536f1bf2532edb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
