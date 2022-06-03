3eb0ef70e7e1068bde0bc7747c9ac0a8e4d2282c
Jack Skiles <jackskiles420@gmail.com>
2022-06-03 17:24:20 -0400

    adding links for topbar icons

--- components/TopBar.js
+++ components/TopBar.js
@@ -14,25 +14,27 @@ const TopBar = () => {
                     </div>
                     <div className={styles.contactdiv}>
                         <img className={styles.topimg} src="Icon awesome-phone-alt.png" />
-                        <p>JackSkiles71@gmail.com</p>
+                        <p>423 619-5332</p>
                     </div>
                 </div>
                 <div className={styles.seperatordiv}></div>
                 <div className={styles.topbarright2}>
                     <div className={styles.contactdiv}>
+                        <a className={styles.imageLink} href="https://www.linkedin.com/in/jack-skiles-46500a1a7/">
                             <img className={styles.topimg2} src="Icon awesome-linkedin.png" />
+                        </a>
                     </div>
                     <div className={styles.contactdiv}>
-                            <img className={styles.topimg2} src="Icon awesome-github.png" />
+                        <img className={styles.topimg2} src="Icon awesome-github.png" />
                     </div>
                     <div className={styles.contactdiv}>
-                            <img className={styles.topimg2} src="Icon ionic-md-paper.png" />
+                        <img className={styles.topimg2} src="Icon ionic-md-paper.png" />
                     </div>
                     <div className={styles.contactdiv2}>
-                            <img className={styles.topimg2mobile} src="mailmobile.png" />
+                        <img className={styles.topimg2mobile} src="mailmobile.png" />
                     </div>
                     <div className={styles.contactdiv2}>
-                            <img className={styles.topimg2mobile} src="phonemobile.png" />
+                        <img className={styles.topimg2mobile} src="phonemobile.png" />
                     </div>
                 </div>
             </div>
